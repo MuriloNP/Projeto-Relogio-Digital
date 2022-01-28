35cf9cdb215ba78a85af61ba6842124a0fb4947f
MuriloNP <murilloprado457@gmail.com>
2022-01-28 16:40:27 -0300

    modified varible hDeg

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let digitalElement = document.querySelector(".digital");
-let sElement = document.querySelector(".p_s");
-let mElement = document.querySelector(".p_m");
-let hElement = document.querySelector(".p_h");
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
 
 function updateClock() {
   let now = new Date();
@@ -15,7 +15,7 @@ function updateClock() {
   0;
   let sDeg = (360 / 60) * seconds - 90;
   let mDeg = (360 / 60) * minutes - 90;
-  let hDeg = (360 / 12) * hours - 90;
+  let hDeg = (360 / 12) * hours + (30 / 60) * minutes - 90;
 
   sElement.style.transform = `rotate(${sDeg}deg)`;
   mElement.style.transform = `rotate(${mDeg}deg)`;
